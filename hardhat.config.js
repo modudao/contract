5ba51a6a609673ffa89020d845ef0228bfa322c4
solidity12 <volibear1001@gmail.com>
2024-06-24 01:56:46 +0900

    feat: first commit

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
+
+const KLAYTN_URL = "https://public-en-cypress.klaytn.net";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [{
+      version: "0.8.24",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 2 ** 32 - 1,
+        }
+      }
+    },],
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      forking: {
+        enabled: true,
+        url: KLAYTN_URL,
+      },
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    klay: {
+      url: KLAYTN_URL,
+      accounts: [process.env.PK],
+    },
+  },
+};
