285b83dd996b2cca22b31334a9440bf0ad2ecf96
solidity12 <volibear1001@gmail.com>
2024-06-25 17:10:51 +0900

    feat: Add deploy nft script

---
+++ scripts/deploy-nft.js
@@ -0,0 +1,19 @@
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const MODUDAONFT = await ethers.getContractFactory("MODUDAONFT", []);
+  const modudaoNFT = await upgrades.deployProxy(MODUDAONFT, ["다오랩 맴버십", "다오랩"], { initializer: "initialize" });
+  await modudaoNFT.waitForDeployment();
+  console.log("MODUDAONFT contract deployed to:", await modudaoNFT.getAddress());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
