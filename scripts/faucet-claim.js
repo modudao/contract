207386a897ada626face577e8bd114e57edb7d68
solidity12 <volibear1001@gmail.com>
2024-09-10 12:22:21 +0900

    feat: add facet witdraw script

---
+++ scripts/faucet-claim.js
@@ -0,0 +1,20 @@
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const faucet = await ethers.getContractAt("Faucet", "0xc85fcd35dcc28e2283354cf4ed5d1f3299a90d18");
+
+    const tx = await faucet.withdraw();
+    await tx.wait();
+    console.log("withdraw hash:", await tx.hash);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
